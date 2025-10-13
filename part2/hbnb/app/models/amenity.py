992bab9b9ae2e88eba31f17c7658a5ed4338eb79
Mathieu7483 <11436@holbertonstudents.com>
2025-10-13 13:22:37 +0200

    Add Amenity model with initialization and method to add amenities

---
+++ part2/hbnb/app/models/amenity.py
@@ -0,0 +1,14 @@
+from hbnb.app.models.basemodel import BaseModel
+
+class Amenity(BaseModel):
+    def __init__(self, id, name, created_at, updated_at):
+        super().__init__()
+        self.id = id
+        self.name = name
+        self.created_at = created_at
+        self.updated_at = updated_at
+        self.amenities = [] #list to store related amenities
+    
+    def add_amenity(self, amenity):
+        """Add an amenity to a place"""
+        self.amenities.append(amenity)
