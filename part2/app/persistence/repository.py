3e15525ae7d13a99dd8810b6d795164b48ee093f
Boubouche1709 <hadjarflorian@gmail.com>
2025-10-15 09:30:28 +0200

    Add repository.py in persistance folder

---
+++ part2/app/persistence/repository.py
@@ -0,0 +1,23 @@
+class InMemoryRepository:
+    def __init__(self):
+        self.storage = {}
+
+    def add(self, obj):
+        self.storage[obj.id] = obj
+
+    def get(self, obj_id):
+        return self.storage.get(obj_id)
+
+    def get_all(self):
+        return list(self.storage.values())
+
+    def update(self, obj_id, data):
+        obj = self.get(obj_id)
+        if obj:
+            obj.update(data)
+
+    def get_by_attribute(self, attr_name, value):
+        for obj in self.storage.values():
+            if hasattr(obj, attr_name) and getattr(obj, attr_name) == value:
+                return obj
+        return None
