8e6e00cc6babb2d3a8671d74fbea7d08d80fa96f
Mathieu7483 <11436@holbertonstudents.com>
2025-10-15 14:07:29 +0200

    Refactor __init__.py to import HBnB_FACADE directly, enhancing clarity and consistency in the code structure

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -1,15 +1,19 @@
 from flask import Flask
 from flask_restx import Api
-from app.api.v1.users import users_ns
-from app.api.v1.amenities import amenities_ns
-from app.api.v1.places import places_ns
-from app.api.v1.reviews import reviews_ns
+from .services.facade import HBnBFacade 
+
+HBnB_FACADE = HBnBFacade()
 
 def create_app():
     app = Flask(__name__)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
 
     # Register the route namespace
+    from .api.v1.users import users_ns
+    from .api.v1.amenities import amenities_ns
+    from .api.v1.places import places_ns
+    from .api.v1.reviews import reviews_ns
+
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(places_ns, path='/api/v1/places')
