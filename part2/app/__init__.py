ffc4d3b04c643110341d18d1d89018edf3bf1683
Boubouche1709 <hadjarflorian@gmail.com>
2025-10-15 09:27:49 +0200

    Fix the file with the namespace

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -2,7 +2,19 @@ from flask import Flask
 from flask_restx import Api
 
 
+from app.api.users import api as users_ns
+from app.api.places import api as places_ns
+from app.api.reviews import api as reviews_ns
+from app.api.amenities import api as amenities_ns
+
+
 def create_app():
     app = Flask(__name__)
-    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
+    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/')
+
+    api.add_namespace(users_ns, path='/api/users')
+    api.add_namespace(places_ns, path='/api/places')
+    api.add_namespace(reviews_ns, path='/api/reviews')
+    api.add_namespace(amenities_ns, path='/api/amenities')
+    
     return app
