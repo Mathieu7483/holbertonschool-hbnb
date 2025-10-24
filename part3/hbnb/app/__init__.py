c1fd60eebaa8c74a6a3baeb5d93ba60b961b42a7
Mathieu7483 <11436@holbertonstudents.com>
2025-10-24 10:07:24 +0200

    Fix duplicate namespace registrations for places, reviews, and amenities in create_app function

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -41,5 +41,8 @@ def create_app(config_class='config.DevelopmentConfig'):
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
+    api.add_namespace(places_ns, path='/api/v1/places')
+    api.add_namespace(reviews_ns, path='/api/v1/reviews')
+    api.add_namespace(amenities_ns, path='/api/v1/amenities')
     
     return app
