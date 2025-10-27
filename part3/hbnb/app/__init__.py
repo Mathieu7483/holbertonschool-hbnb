3d3bb83db8284190c01969aafb10da848aba1f7b
Boubouche1709 <hadjarflorian@gmail.com>
2025-10-27 12:56:36 +0100

    Update API routes

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -7,13 +7,13 @@ HBnB_FACADE = HBnBFacade()
 
 
 def create_app(config_class="config.DevelopmentConfig"):
-   
+
     # Initialize Flask application
     app = Flask(__name__)
-    
+
     # Load configuration from the provided config class
     app.config.from_object(config_class)
-    
+
     # Initialize Flask-RESTX API with Swagger documentation
     api = Api(
         app,
@@ -22,18 +22,18 @@ def create_app(config_class="config.DevelopmentConfig"):
         description='HBnB Application API',
         doc='/api/v2/docs'  # URL for Swagger UI
     )
-    
+
     # Register API namespaces (blueprints)
     from app.api.v2.users import users_ns
     from app.api.v2.places import places_ns
     from app.api.v2.reviews import reviews_ns
     from app.api.v2.amenities import amenities_ns
-    
+
     # Add namespaces without additional prefix
-    api.add_namespace(users_ns, path='/api/v1/users')
-    api.add_namespace(places_ns, path='/api/v1/places')
-    api.add_namespace(reviews_ns, path='/api/v1/reviews')
-    api.add_namespace(amenities_ns, path='/api/v1/amenities')
+    api.add_namespace(users_ns, path='/api/v2/users')
+    api.add_namespace(places_ns, path='/api/v2/places')
+    api.add_namespace(reviews_ns, path='/api/v2/reviews')
+    api.add_namespace(amenities_ns, path='/api/v2/amenities')
 
     # Initialize Bcrypt for password hashing
     bcrypt = Bcrypt()
