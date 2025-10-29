f4ec1e82205e834f919e96373dbe519dc8754137
Mathieu7483 <11436@holbertonstudents.com>
2025-10-29 09:54:43 +0100

    Add protected resource endpoint with JWT token validation

--- part3/hbnb/app/api/v2/auth.py
+++ part3/hbnb/app/api/v2/auth.py
@@ -1,6 +1,6 @@
 from flask_restx import Namespace, Resource, fields
 from flask_jwt_extended import create_access_token
-from flask_jwt_extended import jwt_required, get_jwt_identity
+from flask_jwt_extended import jwt_required, get_jwt_identity, get_jwt
 from datetime import timedelta
 from app.services import facade
 
@@ -43,3 +43,16 @@ class Login(Resource):
 
 
 
+@auth_ns.route('/protected')
+class ProtectedResource(Resource):
+    @jwt_required()
+    def get(self):
+         """A protected endpoint that requires a valid JWT token"""
+         print("jwt------")
+         print(get_jwt_identity())
+         current_user = get_jwt_identity() # Retrieve the user's identity from the token
+         claims = get_jwt()  # Retrieve additional claims if needed
+         return {
+              'message': f'Hello, user {current_user}',
+              "is-admin": claims["is_admin"]
+         }, 200
