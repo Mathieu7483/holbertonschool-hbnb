88c46d131b1b5d0797f8e36c4a4effe0f8422882
Mathieu7483 <11436@holbertonstudents.com>
2025-10-29 09:55:43 +0100

    Refactor app initialization to streamline extension setup and remove unused protected namespace

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -2,14 +2,14 @@ from flask import Flask
 from flask_restx import Api
 from .services.facade import HBnBFacade
 from flask_bcrypt import Bcrypt
-from app.extensions import bcrypt, jwt
-from flask_sqlalchemy import SQLAlchemy
+from app.extensions import bcrypt, jwt, db
+
 
 
 HBnB_FACADE = HBnBFacade()
-db = SQLAlchemy()
 
 def create_app(config_class="config.DevelopmentConfig"):
+    # App Factory function to create and configure the Flask application
 
     # 1. Initialize Flask application
     app = Flask(__name__)
@@ -17,13 +17,10 @@ def create_app(config_class="config.DevelopmentConfig"):
     # 2. Load configuration from the provided config class
     app.config.from_object(config_class)
 
-    # 3. Initialize JWT Manager and Bcrypt 🔒
-    app.config['JWT_SECRET_KEY'] = app.config.get('SECRET_KEY', 'super-secret')
+    # 3.# initialize extensions
     jwt.init_app(app)
-
-    # initialize Bcrypt extension
-    global bcrypt
     bcrypt.init_app(app)
+    db.init_app(app)
 
     # 4. DEFINE Swagger Authorizations
     authorizations = {
@@ -46,16 +43,12 @@ def create_app(config_class="config.DevelopmentConfig"):
         security='jwt'
     )
 
-    # 6. Initialize database
-    db.init_app(app)
-
-    # 7. Register API namespaces (blueprints)
+    # 6. Register API namespaces (blueprints)
     from app.api.v2.users import users_ns
     from app.api.v2.places import places_ns
     from app.api.v2.reviews import reviews_ns
     from app.api.v2.amenities import amenities_ns
     from app.api.v2.auth import auth_ns
-    from app.api.v2.protected import protected_ns
 
     # Add namespaces with prefix
     api.add_namespace(users_ns, path='/api/v2/users')
@@ -63,7 +56,6 @@ def create_app(config_class="config.DevelopmentConfig"):
     api.add_namespace(reviews_ns, path='/api/v2/reviews')
     api.add_namespace(amenities_ns, path='/api/v2/amenities')
     api.add_namespace(auth_ns, path='/api/v2/auth')
-    api.add_namespace(protected_ns, path='/api/v2/protected')
 
 
     return app
