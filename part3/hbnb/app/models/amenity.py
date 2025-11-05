3000929134880246c063963a320c1e9969827d4c
Mathieu7483 <11436@holbertonstudents.com>
2025-11-05 18:25:15 +0100

    Refactor Amenity model: remove unused __init__, validate, and update methods for cleaner code

--- part3/hbnb/app/models/amenity.py
+++ part3/hbnb/app/models/amenity.py
@@ -5,8 +5,6 @@ class Amenity(BaseModel):
     __tablename__ = 'amenities'
     name = db.Column(db.String(128), unique=True, nullable=False)
 
-    # --- SUPPRESSION DE __init__, validate, update ---
-    
     def to_dict(self):
         data = super().to_dict()
         data.update({"name": self.name})
