9134d6c3dacbbd8df3e580a9baf20df641ab8116
Mathieu7483 <11436@holbertonstudents.com>
2025-11-08 16:40:50 +0100

    Enhance Amenity model by adding index to name column and improving serialization method

--- part3/hbnb/app/models/amenity.py
+++ part3/hbnb/app/models/amenity.py
@@ -3,9 +3,14 @@ from app.extensions import db
 
 class Amenity(BaseModel):
     __tablename__ = 'amenities'
-    name = db.Column(db.String(128), unique=True, nullable=False)
+    
+    name = db.Column(db.String(128), unique=True, nullable=False, index=True)
 
     def to_dict(self):
+        """Serialization of Amenity"""
         data = super().to_dict()
         data.update({"name": self.name})
         return data
+
+    def __repr__(self):
+        return f"<Amenity {self.name}>"
